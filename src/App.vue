5e66badf596a0b489090bfc5a24ba1b9478aba77
Yu <546448821@qq.com>
2020-12-25 17:02:20 +0800

    创建个人网站

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!-- 导航栏 -->
+    <nav id="nav-header" class="navbar">
+      <div class="song">
+        <p id="f" @click='fade' class="animated_zoomInLeft">hello</p>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
-
+<script>
+export default {
+  methods: {
+    fade () {
+      console.log(1)
+      this.$('#f').addClass('animated_zoomOutRight')
+    }
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -15,18 +26,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack')
+module.exports = {
+  publicPath: '/',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        assets: '@/assets',
+        common: '@/common',
+        components: '@/components',
+        network: '@/network',
+        views: '@/views'
+      }
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'windows.jQuery': 'jquery'
+      })
+    ]
+  }
+}
